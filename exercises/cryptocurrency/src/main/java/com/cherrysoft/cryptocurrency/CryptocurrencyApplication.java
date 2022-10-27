94a1d7a9ad3a57e681eda289039d9f25fd3366cf
HikingCarrot7 <ricardoibarra2044@gmail.com>
2022-10-27 18:47:17 -0500

    [Ex](crypto): init Spring project

---
+++ exercises/cryptocurrency/src/main/java/com/cherrysoft/cryptocurrency/CryptocurrencyApplication.java
@@ -0,0 +1,13 @@
+package com.cherrysoft.cryptocurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptocurrencyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CryptocurrencyApplication.class, args);
+  }
+
+}
