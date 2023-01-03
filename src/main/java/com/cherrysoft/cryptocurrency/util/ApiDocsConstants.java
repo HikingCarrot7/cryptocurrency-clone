2e0c10b84a6e44de505973a35d6b9b2202bff718
HikingCarrot7 <ricardoibarra2044@gmail.com>
2023-01-02 18:52:01 -0600

    Added ApiDocsConstants class

---
+++ src/main/java/com/cherrysoft/cryptocurrency/util/ApiDocsConstants.java
@@ -0,0 +1,9 @@
+package com.cherrysoft.cryptocurrency.util;
+
+public class ApiDocsConstants {
+  public static final String BAD_REQUEST_RESPONSE_REF = "400ErrorResponse";
+  public static final String NOT_FOUND_RESPONSE_REF = "404ErrorResponse";
+  public static final String UNAUTHORIZED_RESPONSE_REF = "401Response";
+  public static final String FORBIDDEN_RESPONSE_REF = "403Response";
+  public static final String INTERNAL_SERVER_ERROR_RESPONSE_REF = "500Response";
+}
