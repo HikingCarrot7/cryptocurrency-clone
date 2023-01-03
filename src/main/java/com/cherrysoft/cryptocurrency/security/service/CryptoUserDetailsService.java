771b7a93709a2d70d3bda2a236525c09ae20f1e3
HikingCarrot7 <ricardoibarra2044@gmail.com>
2023-01-02 23:44:21 -0600

    Fix: return SecurityCryptoUser instance in loadUserByUsername() method for CryptoUserDetailsService class

--- src/main/java/com/cherrysoft/cryptocurrency/security/service/CryptoUserDetailsService.java
+++ src/main/java/com/cherrysoft/cryptocurrency/security/service/CryptoUserDetailsService.java
@@ -3,9 +3,9 @@ package com.cherrysoft.cryptocurrency.security.service;
 import com.cherrysoft.cryptocurrency.exception.BadCredentialsException;
 import com.cherrysoft.cryptocurrency.exception.CryptoUserNotFoundException;
 import com.cherrysoft.cryptocurrency.model.CryptoUser;
+import com.cherrysoft.cryptocurrency.security.SecurityCryptoUser;
 import com.cherrysoft.cryptocurrency.service.CryptoUserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
@@ -19,11 +19,7 @@ public class CryptoUserDetailsService implements UserDetailsService {
   public UserDetails loadUserByUsername(String username) {
     try {
       CryptoUser cryptoUser = cryptoUserService.getCryptoUserByUsername(username);
-      return User.builder()
-          .username(cryptoUser.getUsername())
-          .password(cryptoUser.getPassword())
-          .authorities("READ")
-          .build();
+      return new SecurityCryptoUser(cryptoUser);
     } catch (CryptoUserNotFoundException e) {
       throw new BadCredentialsException();
     }
